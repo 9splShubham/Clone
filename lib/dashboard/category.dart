a1f525e3db010c1f2cfb1f06e4576f30675b4981
9splShubham <shubham.gogavale@9spl.com>
2023-03-28 19:11:13 +0530

    New

---
+++ lib/dashboard/category.dart
@@ -0,0 +1,111 @@
+import 'package:clone/core/app_image.dart';
+import 'package:clone/core/app_string.dart';
+
+class Category {
+  String? image;
+  String? name;
+  String? price;
+
+  Category({this.image, this.name, this.price});
+
+  Category.fromJson(Map<String, dynamic> json) {
+    image = json['image'];
+    name = json['name'];
+    price = json['price'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+
+    data['image'] = image;
+    data['name'] = name;
+    data['price'] = price;
+    return data;
+  }
+}
+
+List<Category> items() {
+  List<Category> mList = [];
+
+  Category mCategory = Category();
+
+  mCategory.image = AppImage.doritos;
+  mCategory.name = AppString.textDoritostext;
+  mCategory.price = AppString.textRS;
+  mList.add(mCategory);
+
+  mCategory = Category();
+  mCategory.image = AppImage.Hamper;
+  mCategory.name = AppString.textAveenoBaby;
+  mCategory.price = AppString.textRS2;
+  mList.add(mCategory);
+
+  mCategory = Category();
+  mCategory.image = AppImage.Serum;
+  mCategory.name = AppString.textTheOrdinaryHyaluronicAcid;
+  mCategory.price = AppString.textRS3;
+  mList.add(mCategory);
+
+  mCategory = Category();
+  mCategory.image = AppImage.Shampoo;
+  mCategory.name = AppString.textHeadandShouldersShampoo;
+  mCategory.price = AppString.textRS4;
+  mList.add(mCategory);
+
+  return mList;
+}
+
+List<Category> Products() {
+  List<Category> mList = [];
+
+  Category mCategory = Category();
+
+  mCategory.image = AppImage.medicine;
+  mCategory.name = AppString.textTrueBasicsMultivitSport;
+  mCategory.price = AppString.textRS5;
+  mList.add(mCategory);
+
+  mCategory = Category();
+  mCategory.image = AppImage.ponds;
+  mCategory.name = AppString.textdPondVitaminMicellar;
+  mCategory.price = AppString.textRS6;
+  mList.add(mCategory);
+
+  mCategory = Category();
+  mCategory.image = AppImage.formula;
+  mCategory.name = AppString.textFormulaMaskCollection;
+  mCategory.price = AppString.textRS7;
+  mList.add(mCategory);
+
+  mCategory = Category();
+  mCategory.image = AppImage.pampers;
+  mCategory.name = AppString.textBabyDiapersNewborn;
+  mCategory.price = AppString.textRS8;
+  mList.add(mCategory);
+
+  mCategory = Category();
+  mCategory.image = AppImage.medicine;
+  mCategory.name = AppString.textTrueBasicsMultivitSport;
+  mCategory.price = AppString.textRS5;
+  mList.add(mCategory);
+
+  mCategory = Category();
+  mCategory.image = AppImage.ponds;
+  mCategory.name = AppString.textdPondVitaminMicellar;
+  mCategory.price = AppString.textRS6;
+  mList.add(mCategory);
+
+  mCategory = Category();
+  mCategory.image = AppImage.formula;
+  mCategory.name = AppString.textFormulaMaskCollection;
+  mCategory.price = AppString.textRS7;
+  mList.add(mCategory);
+
+  mCategory = Category();
+  mCategory.image = AppImage.pampers;
+  mCategory.name = AppString.textBabyDiapersNewborn;
+  mCategory.price = AppString.textRS8;
+  mList.add(mCategory);
+
+  return mList;
+}
---
+++ lib/recommend_product/recommend_product.dart
@@ -0,0 +1,154 @@
+import 'package:clone/core/app_color.dart';
+import 'package:clone/core/app_fonts.dart';
+import 'package:clone/core/app_image.dart';
+import 'package:clone/core/app_size.dart';
+import 'package:clone/core/app_string.dart';
+import 'package:clone/dashboard/category.dart';
+import 'package:clone/dashboard/dashboard.dart';
+import 'package:flutter/material.dart';
+
+class RecommendProducts extends StatefulWidget {
+  const RecommendProducts({Key? key}) : super(key: key);
+
+  @override
+  State<RecommendProducts> createState() => _RecommendProductsState();
+}
+
+class _RecommendProductsState extends State<RecommendProducts> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        key: scaffoldKey,
+        appBar: AppBar(
+          backgroundColor: AppColor.colorPrimary,
+          elevation: 0,
+          title: Row(
+            children: [
+              IconButton(
+                icon: Image.asset(
+                  AppImage.back2,
+                  height: 30,
+                  fit: BoxFit.fill,
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Dashboard()),
+                  );
+                },
+              ),
+              Text(
+                "Recommend Products",
+                style: getTextStyle(AppFonts.regular, AppSize.textSize18),
+              ),
+              SizedBox(
+                width: 30,
+              ),
+              IconButton(
+                icon: Image.asset(
+                  AppImage.tab,
+                  fit: BoxFit.fill,
+                ),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Image.asset(
+                  AppImage.shop,
+                  fit: BoxFit.fill,
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+        body: const MyStatefulWidget(),
+      ),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: 900,
+            child: GridView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: 8,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 0,
+                  mainAxisSpacing: 0,
+                ),
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Container(
+                        height: 240,
+                        child: Column(
+                          children: [
+                            Container(
+                              height: 100,
+                              child: Image.asset(
+                                Products()[index].image!,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(3),
+                              child: Wrap(
+                                children: [
+                                  Text(
+                                    Products()[index].name!,
+                                    style: getTextStyle(AppFonts.regularBlack2,
+                                        AppSize.textSize14),
+                                  ),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text(
+                                        Products()[index].price!,
+                                        style: getTextStyle(
+                                            AppFonts.regularGreen,
+                                            AppSize.textSize14),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                            // Align(
+                            //   alignment: Alignment.topLeft,
+                            //   child:
+                            // ),
+                          ],
+                        )),
+                  );
+                }),
+          ),
+        ],
+      ),
+    );
+  }
+}
